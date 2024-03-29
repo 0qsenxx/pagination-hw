478d79037dcd592c2bb1a3a0e5344c3ddc8c1ab3
0qsenxx <semen.antipyuk@gmail.com>
2024-03-29 18:30:16 +0200

    fix

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const photosListRef = document.querySelector(".photos__list");
 const loadMoreBtnRef = document.querySelector(".load-more__button");
+const searchPhotosInputRef = document.querySelector(".search-photos__input");
 let currentPage = 1;
+let searchPhotosCurrentPage = 1;
 const searchParams = new URLSearchParams({
   key: "43032297-bb179a9d38920a1e0de24f77d",
   editors_choice: true,
   per_page: 8,
   page: 1,
 });
+let isSearchMode = false;
 
 fetch(`https://pixabay.com/api/?${searchParams}`)
   .then((res) => {
@@ -16,25 +19,84 @@ fetch(`https://pixabay.com/api/?${searchParams}`)
     return res.json();
   })
   .then((data) => {
-    console.log(data);
-    data.hits.forEach((elem) => {
+    data.hits.forEach((photo) => {
       photosListRef.insertAdjacentHTML(
         "beforeend",
-        `<li class="photos__item"><img src="${elem.webformatURL}" class="photo"/></li>`
+        `<li class="photos__item"><img src="${photo.webformatURL}" class="photo"/></li>`
       );
     });
   });
 
-loadMoreBtnRef.addEventListener("click", (evt) => {
-  currentPage++;
+loadMoreBtnRef.addEventListener("click", () => {
+  if (!isSearchMode) {
+    currentPage++;
+    const params = new URLSearchParams({
+      key: "43032297-bb179a9d38920a1e0de24f77d",
+      editors_choice: true,
+      per_page: 8,
+      page: currentPage,
+    });
+    fetch(`https://pixabay.com/api/?${params}`)
+      .then((res) => {
+        if (!res.ok) {
+          console.log("!res.ok");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        if (data.hits.length < 1) {
+          alert("Нажаль більше немає зображень");
+          currentPage = 1;
+        }
+        data.hits.forEach((elem) => {
+          photosListRef.insertAdjacentHTML(
+            "beforeend",
+            `<li class="photos__item"><img src="${elem.webformatURL}" class="photo"/></li>`
+          );
+        });
+      });
+  } else {
+    searchPhotosCurrentPage++;
+    const params = new URLSearchParams({
+      key: "43032297-bb179a9d38920a1e0de24f77d",
+      editors_choice: true,
+      per_page: 8,
+      page: searchPhotosCurrentPage,
+      q: searchPhotosInputRef.value,
+    });
+    fetch(`https://pixabay.com/api/?${params}`)
+      .then((res) => {
+        if (!res.ok) {
+          console.log("!res.ok");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        if (data.hits.length === 0) {
+          alert("Нажаль ми не знайшли зображень");
+          loadMoreBtnRef.setAttribute("disabled", true);
+        }
+        data.hits.forEach((elem) => {
+          photosListRef.insertAdjacentHTML(
+            "beforeend",
+            `<li class="photos__item"><img src="${elem.webformatURL}" class="photo"/></li>`
+          );
+        });
+      });
+  }
+});
+
+searchPhotosInputRef.addEventListener("input", (evt) => {
+  isSearchMode = true;
+  photosListRef.innerHTML = ``;
+  searchPhotosCurrentPage = 1;
   const params = new URLSearchParams({
     key: "43032297-bb179a9d38920a1e0de24f77d",
     editors_choice: true,
     per_page: 8,
-    page: currentPage,
+    page: searchPhotosCurrentPage,
+    q: evt.target.value,
   });
-  const url = `https://pixabay.com/api/?${searchParams}`;
-
   fetch(`https://pixabay.com/api/?${params}`)
     .then((res) => {
       if (!res.ok) {
@@ -43,7 +105,9 @@ loadMoreBtnRef.addEventListener("click", (evt) => {
       return res.json();
     })
     .then((data) => {
-      console.log(data);
+      if (data.hits.length < 1) {
+        alert("Нажаль ми не знайшли зображень");
+      }
       data.hits.forEach((elem) => {
         photosListRef.insertAdjacentHTML(
           "beforeend",
